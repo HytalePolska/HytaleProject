0a6838bf43eee744f865db7ffdc8df13ad0eb81c
jacek2000le <jacek2000le@gmail.com>
2020-01-22 13:22:40 +0100

    Update R_Player.js

--- WebService_NodeJSExpress/roudes/R_Player.js
+++ WebService_NodeJSExpress/roudes/R_Player.js
@@ -16,35 +16,36 @@ router.get('/:UUID', (req, res, next) => {
     let cond =[];
     cond['Nick'] = req.params.UUID;
     query=query_Builder.Select("*","MC_Players").Where(cond).Get();
+    console.log( req.params.UUID);
     Con_MySQL.Execute(query, res);
 });
-router.put('/:UUID/:Password/:Nick', (req, res, next) => {
+///:UUID/:Password/:Nick
+router.put('/', (req, res, next) => {
     let con = [];
-    con["Player_ID"]=req.params.UUID ;
-    con["Player_Password"]=req.params.Password ;
-    con["Nick"]=req.params.UUID ;
-    let query = query_Builder.Insert("MC_Players",con).Get(); 
-    Con_MySQL.Execute(query, res);
-   
+    con["Player_ID"]='1232344334';
+    con["Player_Password"]='qaz3421';
+    con["Nick"]='pawel433' ;
+    let query = query_Builder.Insert(con,"MC_Players").Get(); 
+   Con_MySQL.Execute(query, res);
+   // res.send(query);
 });
-router.post('/:UUID/:Password', (req, res, next) => {
-
-
+router.post('/', (req, res, next) => {
     let con = [];
-    con["Player_ID"]="#123#";
+    con["Player_ID"]="#1322323#";
     con["Player_Password"] ="Zeb123";
-    con["Nick"]="Pawel12";
+    con["Nick"]="Pawel4";
 
     let wher = [];
     wher["Player_ID"]="#123#";
+
     let query=query_Builder.Update(con,"MC_Players").Where(wher).Get();
-    Con_MySQL.Execute(query, res);
+   Con_MySQL.Execute(query, res);
 });
 router.delete('/:UUID', (req, res, next) => {
    let con = [];
    con["Nick"]=req.params.UUID ;
    let query = query_Builder.Delete("MC_Players").Where(con).Get(); 
-   Con_MySQL.Execute(query, res);
+  Con_MySQL.Execute(query, res);
 });
 
 

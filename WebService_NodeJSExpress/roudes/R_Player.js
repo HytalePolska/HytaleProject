51afefe55fefff9c2fe25b2478411a02bc563ceb
jacek2000le <jacek2000le@gmail.com>
2020-01-23 14:56:03 +0100

    w

--- WebService_NodeJSExpress/roudes/R_Player.js
+++ WebService_NodeJSExpress/roudes/R_Player.js
@@ -1,18 +1,14 @@
 
 const express = require("express");
+
 const router = express.Router();
 
 const Player = require("../classes/Player");
 
-const SQL_Builder = require("../Tools/Sql_Builder");
-const query_Builder = new SQL_Builder();
-
 const SQL_query = require('../Connectors/MySql_Connector');
-const Table_Name = "MC_Players";
 
 
 router.get('/', async (req, res, next) => {
-    
   await  Player.GET(SQL_query,req.body,res);
 });
 
@@ -26,12 +22,9 @@ router.put('/', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
     await  Player.POST(SQL_query,req.body,res);
-  
-    
 });
-
 router.delete('/:Player_ID', async (req, res, next) => {
-    await  Player.DELETE(SQL_query,req.params,res);
+    await  Player.DELETE(SQL_query,req.params,res); 
 });
 
 

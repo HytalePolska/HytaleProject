8627713b8051728f87facddfaf02eba625501b71
jacek2000le <jacek2000le@gmail.com>
2020-01-23 13:04:54 +0100

    ww

--- WebService_NodeJSExpress/roudes/R_Player.js
+++ WebService_NodeJSExpress/roudes/R_Player.js
@@ -25,29 +25,13 @@ router.put('/', async (req, res, next) => {
 });
 
 router.post('/', async (req, res, next) => {
-
+    await  Player.POST(SQL_query,req.body,res);
   
     
-    let con = [];
-    con["Nick"] = req.body.Nick;
-    con["Player_Password"] = req.body.Player_Password;
-    let wher = [];
-    wher["Player_ID"] = req.body.Player_ID;
-    let query = query_Builder.Select("*", Table_Name).Where(wher).Get();
-    if (JSON.stringify(await SQL_query(query)) == "[]")  //in other case return filled JSON with data
-     res.send("The Player is not existing");
-
-    query = query_Builder.Update(con, Table_Name).Where(wher).Get();
-     await SQL_query(query);
-    res.send("Player has been updated");
 });
 
 router.delete('/:Player_ID', async (req, res, next) => {
-    let con = [];
-    con["Player_ID"] = req.params.Player_ID;
-    let query = query_Builder.Delete(Table_Name).Where(con).Get();
-    await SQL_query(query);
-    res.send("Player has been deleted");
+    await  Player.DELETE(SQL_query,req.params,res);
 });
 
 

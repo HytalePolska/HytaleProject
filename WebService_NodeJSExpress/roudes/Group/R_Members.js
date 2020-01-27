d0539d6e09c01a3f5ce6971edffc93a3a9513a1d
jacek2000le <jacek2000le@gmail.com>
2020-01-27 13:14:17 +0100

    Update R_Members.js

--- WebService_NodeJSExpress/roudes/Group/R_Members.js
+++ WebService_NodeJSExpress/roudes/Group/R_Members.js
@@ -8,11 +8,15 @@ const Member = require("../../Classes/Group/Member");
 const SQL_query = require('../../Connectors/MySql_Connector');
 
 router.get('/:M_GroupID', async (req, res, next) => {
-    await Member.GET(SQL_query, req.params, res);
+    console.log("Witam"+req.params.M_GroupID);
+      res.send("w");
+  //  await Member.GET(SQL_query, req.params, res);
 });
 
 router.get('/M_GroupID:M_PlayerID', async (req, res, next) => {
-    await Member.GET(SQL_query, req.params, res);
+    console.log("Witam" +res.params.M_PlayerID +" "+res.params.M_GroupID);
+    res.send(res.params.M_PlayerID +" "+res.params.M_GroupID);
+  //  await Member.GET(SQL_query, req.params, res);
 });
 
 router.put('/', async (req, res, next) => {

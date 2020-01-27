56885f45af45e28b1727546d79eec43e30fcd77c
jacek2000le <jacek2000le@gmail.com>
2020-01-27 15:54:06 +0100

    3

--- WebService_NodeJSExpress/roudes/Group/R_Groups.js
+++ WebService_NodeJSExpress/roudes/Group/R_Groups.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 const Group = require("../../Classes/Group/Group");
 
--- WebService_NodeJSExpress/roudes/Group/R_Members.js
+++ WebService_NodeJSExpress/roudes/Group/R_Members.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 const Member = require("../../Classes/Group/Member");
 

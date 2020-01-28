13c90b2ef7256abbc79f5e61f87e9a1db8f5e23a
jacek2000le <jacek2000le@gmail.com>
2020-01-28 16:01:03 +0100

    Update MySql_Connector.js

--- WebService_NodeJSExpress/Connectors/MySql_Connector.js
+++ WebService_NodeJSExpress/Connectors/MySql_Connector.js
@@ -1,16 +1,28 @@
 
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-    connectionLimit: 10,
-    password: '0fecf3c613dffed104bf',
-    user: 'csrv_708300',
-    database: 'csrv_708300',
-    host: 'mysql.craftserve.pl',
-    post: '3306'
-});
+const db = handleDisconnect();
 
 
+function handleDisconnect() 
+{
+    return  mysql.createConnection({
+        connectionLimit: 10,
+        password: '0fecf3c613dffed104bf',
+        user: 'csrv_708300',
+        database: 'csrv_708300',
+        host: 'mysql.craftserve.pl',
+        post: '3306'
+    });
+}
+db.on('error',function(err){
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      db =  handleDisconnect();  
+      console.log("Rozłączono z bazą danych próba podłączenia");                       // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+        throw err;                                  // server variable configures this)
+      }
+})
 
 const Sql_Query = async (query) => new Promise(
     (resolve, reject) => {

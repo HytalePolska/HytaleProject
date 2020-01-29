126abfe4af987135007122eb2c107c4b7cd26283
jacek2000le <jacek2000le@gmail.com>
2020-01-29 08:29:23 +0100

    Update R_Premmission.js

--- WebService_NodeJSExpress/Plugins/Premissions/R_Premmission.js
+++ WebService_NodeJSExpress/Plugins/Premissions/R_Premmission.js
@@ -15,7 +15,7 @@ const SQL_query = require('../../Connectors/MySql_Connector');
 router.get('/', async (req, res, next) => {
     await Group.GET(SQL_query,req.query, res);
 });
-router.put('/:GroupID', async (req, res, next) => {
+router.put('/', async (req, res, next) => {
     await Group.PUT(SQL_query, req.body, res);
 });
 
@@ -27,6 +27,22 @@ router.delete('/:GroupID', async (req, res, next) => {
     await Group.DELETE(SQL_query, req.params, res);
 });
 
+router.get('/:GroupID/players/:PlayerID', async (req, res, next) => {
+    console.log(req.params);
+    await Group.GET(SQL_query,req.query, res);
+});
+router.put('/:GroupID/players', async (req, res, next) => {
+    await Group.PUT(SQL_query, req.body, res);
+});
+
+router.post('/:GroupID/players', async (req, res, next) => {
+    await Group.POST(SQL_query, req.body, res);
+});
+
+router.delete('/:GroupID/players/:PlayerID', async (req, res, next) => {
+    console.log(req.params);
+    await Group.DELETE(SQL_query, req.params, res);
+});
 
 
 

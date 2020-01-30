35aae80cf5f21f655d2e0f7750b80408979fbf45
jacekxpl89 <52602529+jacekxpl89@users.noreply.github.com>
2020-01-30 20:46:37 +0100

    s

--- WebService_NodeJSExpress/Plugins/Login/R_Login.js
+++ WebService_NodeJSExpress/Plugins/Login/R_Login.js
@@ -9,75 +9,69 @@ const SQL_builder = require('../../Tools/Sql_Builder');
 const Config = require("./config");
 //login
 router.post('/', async (req, res, next) => {
-    let reqbody = JSON.parse(JSON.stringify(req.body))[0];
+  let reqbody = JSON.parse(JSON.stringify(req.body))[0];
 
-    let player  = await Player.GET(SQL_query,reqbody);
+  let player = await Player.GET(SQL_query, reqbody);
 
-    if(typeof player == 'undefined')
-    {
-       res.status(401).send("This players has not established password");
-       return;
-    }
-    if( player.P_Pass !== reqbody.P_Pass)
-    {
-        res.status(402).send("Wrong Password");
-        return;  
-    }
-      player.P_Online = 1;
-      await Player.POST(SQL_query,player);
-      res.status(400).send("Success login");   
+  if (typeof player == 'undefined') {
+    res.status(401).send("This players has not established password");
+    return;
+  }
+  if (player.P_Pass !== reqbody.P_Pass) {
+    res.status(402).send("Wrong Password");
+    return;
+  }
+  player.P_Online = 1;
+  await Player.POST(SQL_query, player);
+  res.status(400).send("Success login");
 });
 //PlayerQuitEvent
-router.post('/Exit/:PlayerID',async(req,res) =>{
-  
-    let player = await Player.GET(SQL_query,req.params);
-    if(typeof player == 'undefined')
-    {
-       res.status(401).send("This players in not exisitng");
-       return;
-    }
+router.post('/Exit/:PlayerID', async (req, res) => {
 
-    player.P_Online =0;
-    await Player.POST(SQL_query,player);
-    res.status(401).send("Player has been logined out");
+  let player = await Player.GET(SQL_query, req.params);
+  if (typeof player == 'undefined') {
+    res.status(401).send("This players in not exisitng");
+    return;
+  }
+
+  player.P_Online = 0;
+  await Player.POST(SQL_query, player);
+  res.send("Player has been logined out");
 });
 //haslo
-router.put('/', async(req,res)=>{
-   let reqbody = JSON.parse(JSON.stringify(req.body))[0];
-    
-    let player  = await Player.GET(SQL_query,reqbody);
-         
-    if(typeof player !== 'undefined')
-    {
-       res.status(401).send("This players has already established password");
-       return;
-    }
-    let Valid_Status = IsPasswordValid(reqbody.P_Pass);
-    if(Valid_Status != true)
-    {
-        res.status(402).send(Valid_Status);
-        return;  
-    }
-    
-    let new_player = [];
-     new_player["PlayerID"] = reqbody.PlayerID;
-     new_player["P_Pass"] = reqbody.P_Pass;
-     new_player["P_Name"] = reqbody.P_Name;
-     new_player["P_Online"] = 0;
+router.put('/', async (req, res) => {
+  let reqbody = JSON.parse(JSON.stringify(req.body))[0];
 
-      await Player.PUT(SQL_query,new_player);
-      res.status(400).send("Successed created account! Now you need to login ");   
+  let player = await Player.GET(SQL_query, reqbody);
+
+  if (typeof player !== 'undefined') {
+    res.status(401).send("This players has already established password");
+    return;
+  }
+  let Valid_Status = IsPasswordValid(reqbody.P_Pass);
+  if (Valid_Status != true) {
+    res.status(402).send(Valid_Status);
+    return;
+  }
+
+  let new_player = [];
+  new_player["PlayerID"] = reqbody.PlayerID;
+  new_player["P_Pass"] = reqbody.P_Pass;
+  new_player["P_Name"] = reqbody.P_Name;
+  new_player["P_Online"] = 0;
+
+  await Player.PUT(SQL_query, new_player);
+  res.status(400).send("Successed created account! Now you need to login ");
 });
-function IsPasswordValid(password)
-{
+function IsPasswordValid(password) {
   let passSize = String(password).length;
-  
-  if(passSize <Config.P_MinSize)
+
+  if (passSize < Config.P_MinSize)
     return "Your password is too short";
-  if(passSize> Config.P_MaxSize)
+  if (passSize > Config.P_MaxSize)
     return "Your password is too large";
 
-    return true;
+  return true;
 }
 
 module.exports = router;

1a32df02ed24c990b0c19bcbad83c80c7f6cb507
jacekxpl89 <52602529+jacekxpl89@users.noreply.github.com>
2020-02-19 23:56:07 +0100

    Update R_Login.js

--- WebServiceMongoDB/Plugins/Login/R_Login.js
+++ WebServiceMongoDB/Plugins/Login/R_Login.js
@@ -5,8 +5,8 @@ const Config = require("./config");
 const plugin = require("../../Controlers/C_Plugin");
 
 
-let plug = { "P_Name":"login","P_Description":"Basic plugin helps players to create accounts"};
-let list = {plug}
+let plug = { "P_Name": "login", "P_Description": "Basic plugin helps players to create accounts" };
+let list = { plug }
 plugin.INSERT(list);
 
 
@@ -54,28 +54,28 @@ router.post('/register', async (req, res) => {
     let data = JSON.parse(JSON.stringify(req.body))[0];
     let p_id = { PlayerID: data["PlayerID"] };
 
-    let player =  await user.EDIT(p_id);
- 
+    let player = await user.EDIT(p_id);
+
     if (player != null) {
-        res.status(401).send("This players is exisitng");
+        res.status(401).send("This password is already set");
         return;
     }
-   
-    let Valid_Status = IsPasswordValid(data.P_Pass);
-  if (Valid_Status != true) {
-    res.status(406).send(Valid_Status);
-    return;
-  }
-  
-  let new_player = [];
-  new_player["PlayerID"] = data.PlayerID;
-  new_player["P_Pass"] = data.P_Pass;
-  new_player["P_Name"] = data.P_Name;
-  new_player["P_Online"] = 1;
-   let list = {new_player};
-    await user.INSERT(list,res);
 
-   
+    let Valid_Status = IsPasswordValid(data.P_Pass);
+    if (Valid_Status != true) {
+        res.status(406).send(Valid_Status);
+        return;
+    }
+
+    let new_player = [];
+    new_player["PlayerID"] = data.PlayerID;
+    new_player["P_Pass"] = data.P_Pass;
+    new_player["P_Name"] = data.P_Name;
+    new_player["P_Online"] = 1;
+    let list = { new_player };
+    await user.INSERT(list, res);
+
+
 });
 router.post('/password', async (req, res) => {
     let data = JSON.parse(JSON.stringify(req.body))[0];
@@ -101,12 +101,12 @@ router.post('/password', async (req, res) => {
 
 function IsPasswordValid(password) {
     let passSize = String(password).length;
-  
+
     if (passSize < Config.P_MinSize)
-      return "Your password is too short";
+        return "Your password is too short";
     if (passSize > Config.P_MaxSize)
-      return "Your password is too large";
-  
+        return "Your password is too large";
+
     return true;
-  }
+}
 module.exports = router;
